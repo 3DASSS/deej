50c0303a5b9d59cf0f26921e435c9dec746a51fd
Nikita <me@nik9.dev>
2026-08-27 12:56:29 +0300

    use .ico icon for windows tray

--- pkg/icon/icon_windows.go
+++ pkg/icon/icon_windows.go
@@ -4,5 +4,5 @@ import _ "embed"
 
 // TrayDeejLogo is a binary representation of the deej logo; used for notifications and tray icon.
 // PNG, because wails' systray icon loader accepts PNG data but not full .ico containers
-//go:embed assets/tray-icon.png
+//go:embed assets/tray-icon.ico
 var TrayDeejLogo []byte
